30e116e092ba9cfd506bc09bbbb8b93c82f6d032
AbdurRob <abdurrobr34l@gmail.com>
2025-07-22 10:44:51 +0600

    Array chapter started and also first practice is complete

---
+++ Basic/array.js
@@ -0,0 +1,33 @@
+// *JavaScript Array Problem for Practice:
+console.log("Array Problem ---------- 1")
+
+// // 1. You create an array, where you put the numbers from 71 to 79.
+// let numbers = [71, 72, 73, 74, 75, 76, 77, 78, 79];
+// console.log(numbers.length + " values inside this array", numbers);
+
+// // 2. You have a small refrigerator at home that can hold exactly 5 fruits. You make an array and put apples, bananas, oranges, mangoes, and guavas in it.
+// let fruits = ['apples', 'bananas', 'oranges', 'mangoes', 'guavas'];
+// console.log(fruits.length + " fruits inside this array", fruits);
+
+// // 3. Suppose you went to the market one day. You put 10 vegetables in your bag: potato, onion, brinjal, radish, carrot, tomato, eggplant, gourd, cucumber and pumpkin. Write a program to find out how many vegetables are in the bag.
+// let vegetables = ['potato', 'onion', 'brinjal', 'radish', 'carrot', 'tomato', 'eggplant', 'gourd', 'cucumber', 'pumpkin'];
+// console.log(vegetables.length + " vegetables inside this array");
+
+// // 4. Put the names of your 5 favorite movies in an array.
+// let movies = ['none', 'none', 'none', 'none', 'none'];
+// console.log(movies);
+
+// // 5. Write an array that contains the odd numbers between 11 and 30.
+// let oddNumbers = [11, 13, 15, 17, 19, 21, 23, 25, 27, 29];
+// console.log(oddNumbers);
+
+// // 6. Make an array of the colors you can see around you. Then log the length of that array to the console.
+// let colorsAroundMe = ['black', 'white', 'lightblue', 'lightyellow'];
+// console.log(colorsAroundMe);
+
+// // 7. Take the names of the capitals of a few countries, put them in an array, and log their length to the console.
+// let capitalsOfCountry = ['Dhaka', 'Lahore', 'Tehran', 'Moscow', 'East Jerusalem'];
+// console.log(capitalsOfCountry.length);
+
+// *JavaScript Array Problem for Practice:
+console.log("Array Problem ---------- 2")
