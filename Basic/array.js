1f522f1d6c4e8640fd394e9c58fb31874fd2fd31
AbdurRob <abdurrobr34l@gmail.com>
2025-07-22 12:29:50 +0600

    Completed Push-pop & Shift, unshift topic of array chapter

--- Basic/array.js
+++ Basic/array.js
@@ -1,33 +1,118 @@
 // *JavaScript Array Problem for Practice:
-console.log("Array Problem ---------- 1")
+// {
+//   console.log("Array Problem ---------- 1")
 
-// // 1. You create an array, where you put the numbers from 71 to 79.
-// let numbers = [71, 72, 73, 74, 75, 76, 77, 78, 79];
-// console.log(numbers.length + " values inside this array", numbers);
+//   // 1. You create an array, where you put the numbers from 71 to 79.
+//   let numbers = [71, 72, 73, 74, 75, 76, 77, 78, 79];
+//   console.log(numbers.length + " values inside this array", numbers);
 
-// // 2. You have a small refrigerator at home that can hold exactly 5 fruits. You make an array and put apples, bananas, oranges, mangoes, and guavas in it.
-// let fruits = ['apples', 'bananas', 'oranges', 'mangoes', 'guavas'];
-// console.log(fruits.length + " fruits inside this array", fruits);
+//   // 2. You have a small refrigerator at home that can hold exactly 5 fruits. You make an array and put apples, bananas, oranges, mangoes, and guavas in it.
+//   let fruits = ['apples', 'bananas', 'oranges', 'mangoes', 'guavas'];
+//   console.log(fruits.length + " fruits inside this array", fruits);
 
-// // 3. Suppose you went to the market one day. You put 10 vegetables in your bag: potato, onion, brinjal, radish, carrot, tomato, eggplant, gourd, cucumber and pumpkin. Write a program to find out how many vegetables are in the bag.
-// let vegetables = ['potato', 'onion', 'brinjal', 'radish', 'carrot', 'tomato', 'eggplant', 'gourd', 'cucumber', 'pumpkin'];
-// console.log(vegetables.length + " vegetables inside this array");
+//   // 3. Suppose you went to the market one day. You put 10 vegetables in your bag: potato, onion, brinjal, radish, carrot, tomato, eggplant, gourd, cucumber and pumpkin. Write a program to find out how many vegetables are in the bag.
+//   let vegetables = ['potato', 'onion', 'brinjal', 'radish', 'carrot', 'tomato', 'eggplant', 'gourd', 'cucumber', 'pumpkin'];
+//   console.log(vegetables.length + " vegetables inside this array");
 
-// // 4. Put the names of your 5 favorite movies in an array.
-// let movies = ['none', 'none', 'none', 'none', 'none'];
-// console.log(movies);
+//   // 4. Put the names of your 5 favorite movies in an array.
+//   let movies = ['none', 'none', 'none', 'none', 'none'];
+//   console.log(movies);
 
-// // 5. Write an array that contains the odd numbers between 11 and 30.
-// let oddNumbers = [11, 13, 15, 17, 19, 21, 23, 25, 27, 29];
-// console.log(oddNumbers);
+//   // 5. Write an array that contains the odd numbers between 11 and 30.
+//   let oddNumbers = [11, 13, 15, 17, 19, 21, 23, 25, 27, 29];
+//   console.log(oddNumbers);
 
-// // 6. Make an array of the colors you can see around you. Then log the length of that array to the console.
-// let colorsAroundMe = ['black', 'white', 'lightblue', 'lightyellow'];
-// console.log(colorsAroundMe);
+//   // 6. Make an array of the colors you can see around you. Then log the length of that array to the console.
+//   let colorsAroundMe = ['black', 'white', 'lightblue', 'lightyellow'];
+//   console.log(colorsAroundMe);
 
-// // 7. Take the names of the capitals of a few countries, put them in an array, and log their length to the console.
-// let capitalsOfCountry = ['Dhaka', 'Lahore', 'Tehran', 'Moscow', 'East Jerusalem'];
-// console.log(capitalsOfCountry.length);
+//   // 7. Take the names of the capitals of a few countries, put them in an array, and log their length to the console.
+//   let capitalsOfCountry = ['Dhaka', 'Lahore', 'Tehran', 'Moscow', 'East Jerusalem'];
+//   console.log(capitalsOfCountry.length);
+// }
 
-// *JavaScript Array Problem for Practice:
-console.log("Array Problem ---------- 2")
+// *JavaScript Array Index for Practice:
+// {
+//   console.log("Array Problem ---------- 2")
+
+//   // 1. You have put all your friends' names in an array. Now you find out who is at index number 3.
+//   let myFriends = ['Minhaz', 'Maahi', 'Shariar', 'Noor', 'Zahid', 'Sami', 'Sadrib'];
+//   console.log(myFriends[3]);
+
+//   // 2. Put the names of your 7 favorite books in an array. Then see what the name of the book in position 5 is.
+//   let favouriteBooks = ['Tintin', 'Js by Jhankar', 'Shariar', 'No more', 'No more', 'No more', 'No more'];
+//   console.log(favouriteBooks[5]);
+
+//   // 3. You created an array containing the numbers 1 to 10. Now update the value at position 7 with 30.
+//   let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//   let updatedNumber = (number[6] = 30);
+//   console.log(number);
+
+//   // 4. You have some games on your mobile and you have put them in an array - 'Free Fire', 'PUBG', 'Candy Crush', 'Temple Run'. Now you want to change the game at index number 2 and put 'Subway Surfer there. Write a program and do that. ("I WILL ADD MY GAMES")
+
+//   let myGames = ['Pubg', 'COD Mobile', 'Apex Legend', 'Delta Force', 'Free Fire', 'PES', 'Super Sus'];
+//   myGames[2] = "It is no more available";
+//   console.log(myGames);
+
+//   // 5. Put the names of your family members in an array. Then output the name of the member in the third position.
+//   let familyMembers = ['Dad', 'Mom', 'Elder Brother', '2nd Elder Brother', 'Younger Brother', 'Last Younger Brother'];
+//   console.log(familyMembers[2]);
+
+//   // 6. You plan to visit 7 countries. Put the names of the countries in an array. Then find the name of the country in position 4.
+//   let countries = ['Saudi Arab', 'Oman', 'Russia', 'Palestine', 'No More', 'No More', 'No More'];
+//   console.log(countries[3]);
+
+//   // 7. There are 4 things on your desk. Put their names in an array. Then find out what the thing in position 7 is.
+//   let thingsOnMyDesk = ['Laptop', 'Keyboard', 'Mini-Bluetooth Speaker', 'Mouse'];
+//   console.log(thingsOnMyDesk[7]);
+// }
+
+// *JavaScript Push-pop Methods Index for Practice:
+// {
+//   console.log("Push-pop Methods Problem ---------- 3")
+
+// // !IMPORTANT NOTE
+//   // ?push(value) → Adds at end
+//   // ?pop() → Removes from end
+  
+//   // ?unshift(value) → Adds at beginning
+//   // ?shift() → Removes from beginning
+
+
+//   // 1. There is an array of numbers: [10, 20, 30, 40, 50]. Now add 60 to this array and output the array.
+//   let numbersArray = [10, 20, 30, 40, 50];
+//   let newNum = numbersArray.push(60);
+
+//   console.log(numbersArray);
+
+//   // 2. You are writing some names in an array: 'Sajeeb', 'Sagar', 'Sakib', 'Sohel'. Now you feel like you need to add a new friend 'Suman' to this array. Write a program to add Suman at the end.
+//   let names = ['Sajeeb', 'Sagar', 'Sakib', 'Sohel'];
+//   let newFriend = names.push('Suman');
+
+//   console.log(names);
+
+//   // 3. Put all the games installed on your mobile in an array. Then remove the game name given at the end. Then, display the remaining games in your array as output.
+//   let gamesInstalled = ['Pubg', 'COD Mobile', 'Apex Legend'];
+//   let removeGame = gamesInstalled.pop();
+
+//   console.log(gamesInstalled);
+
+//   // 4. In this array [24, 36, 48, 60]. Add 12 as the very first element and output the entire array.
+//   let evenNumbers = [24, 36, 48, 60];
+//   let addingNumber = evenNumbers.unshift(12);
+
+//   console.log(evenNumbers);
+
+//   // 5. You have an array with 5 book names. Now you have finished reading the first book. So remove the first book name from the array. Then display the remaining names in the output.
+//   let finishReading = ['Book1', 'Book2', 'Book3', 'Book4', 'Book5'];
+//   let removeBook = finishReading.shift();
+
+//   console.log(finishReading);
+
+// }
+
+// *JavaScript Push-pop Methods Index for Practice:
+{
+  console.log("Push-pop Methods Problem ---------- 4")
+
+}
